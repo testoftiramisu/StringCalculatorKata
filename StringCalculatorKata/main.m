310065fef678956777557655a0a2853ed575beb5
Denys Khlivnyy <testoftiramisu@gmail.com>
2016-01-15 19:23:57 +0100

    Initial Commit

---
+++ StringCalculatorKata/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  StringCalculatorKata
+//
+//  Created by Denys Khlivnyy on 1/15/16.
+//  Copyright © 2016 Denys Khlivnyy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
