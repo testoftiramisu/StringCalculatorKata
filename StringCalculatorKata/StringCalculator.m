f8a3ca3ebd7e83d7f4873dce4efb061e985b9475
Denys Khlivnyy <testoftiramisu@gmail.com>
2016-01-16 22:23:01 +0100

    Covering of non comma delimiters and negative numbers

--- StringCalculatorKata/StringCalculator.m
+++ StringCalculatorKata/StringCalculator.m
@@ -10,12 +10,37 @@
 
 @implementation StringCalculator
 
+- (void)checkForNegative:(NSString *)numbers {
+    NSMutableArray  *chars = [NSMutableArray array];
+    [numbers enumerateSubstringsInRange: NSMakeRange(0, [numbers length])
+                                options: NSStringEnumerationByComposedCharacterSequences
+                             usingBlock: ^(NSString *inSubstring, NSRange inSubstringRange, NSRange inEnclosingRange, BOOL *outStop) {
+                                 [chars addObject: inSubstring];
+                             }];
+    
+    for (NSString *character in chars) {
+        if ([character isEqual: @"-"]) {
+            [NSException raise:NSInvalidArgumentException format: @"Numbers should be positive."];
+        }
+        
+    }
+}
+
+- (NSString *)replaceNonCommaDelimiters:(NSString *)numbers {
+    NSCharacterSet *nonComma = [[NSCharacterSet decimalDigitCharacterSet] invertedSet];
+    numbers = [[numbers componentsSeparatedByCharactersInSet: nonComma] componentsJoinedByString: @","];
+    return numbers;
+}
+
 -(NSInteger)add:(NSString *)numbers{
     NSInteger result = 0;
     
     if ([numbers length] == 0) {
-        return result = 0;
+        return result;
     }
+    
+    [self checkForNegative:numbers];
+    numbers = [self replaceNonCommaDelimiters:numbers];
     
     NSArray *array= [[NSArray alloc] init];
     array = [numbers componentsSeparatedByString:@","];
--- StringCalculatorKataTests/StringCalculatorKataTests.m
+++ StringCalculatorKataTests/StringCalculatorKataTests.m
@@ -20,7 +20,6 @@ StringCalculator *stringCalculator;
 - (void)setUp {
     [super setUp];
     stringCalculator = [[StringCalculator alloc] init];
-    
 }
 
 - (void)tearDown {
@@ -37,17 +36,35 @@ StringCalculator *stringCalculator;
     NSInteger result = [stringCalculator add: @"1"];
     NSInteger expected = 1;
     XCTAssertEqual(expected, result);
-    
 }
 
 - (void)testAddTwoCommaDelimitedNumbersReturnSum {
     NSInteger result = [stringCalculator add: @"1, 1"];
     NSInteger expected = 2;
     XCTAssertEqual(expected, result);
-    
 }
 
+- (void)testAddMultipleCommaDelimitedNumbersReturnSum {
+    NSInteger result = [stringCalculator add: @"1, 2,,, 3"];
+    NSInteger expected = 6;
+    XCTAssertEqual(expected, result);
+}
 
+- (void)testAddNewLineDelimitedNumbersReturnSum {
+    NSInteger result = [stringCalculator add: @"3\n\n3\n3"];
+    NSInteger expected = 9;
+    XCTAssertEqual(expected, result);
+}
+
+- (void)testUserSpecifiedDelimitedNumbersReturnSum {
+    NSInteger result = [stringCalculator add: @"!2@2#2$2%2^2&2*2(2)2,2_2=2+2:2;2'2.2/2?2`2~2\"2|2{2[2}2]2<2>2"];
+    NSInteger expected = 60;
+    XCTAssertEqual(expected, result);
+}
+
+- (void)testAddSingleNegativeNumberThrowsAnException {
+    XCTAssertThrowsSpecificNamed([stringCalculator add: @"-1, 4"], NSException, NSInvalidArgumentException);
+}
 
 
 @end
